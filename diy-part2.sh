798afa3093a191faaff320a187501dc4631deeea
Avantyjw <yujiawei968@qq.com>
2025-02-05 08:54:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,14 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 # Modify hostname
-#sed -i 's/LEDE/Cudy/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/Cudy/g' package/base-files/luci2/bin/config_generate
 
 # Modify default SSID name
-#sed -i 's/LEDE/Cudy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#sed -i 's/US/CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/LEDE/Cudy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/US/CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
